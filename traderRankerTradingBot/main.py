343aab4c388d62ce6ce70b121ce8558ece1e1f58
amuller18 <97063025+amuller18@users.noreply.github.com>
2025-09-23 16:00:02 -0600

    auto restart

--- traderRankerTradingBot/main.py
+++ traderRankerTradingBot/main.py
@@ -2,6 +2,8 @@ import asyncio
 from TelegramToDiscordBot import TelegramToDiscordBot
 from threading import Thread
 import time
+import requests
+from config import Config
 
 
 # Function to run the Telegram bot with a new event loop in a separate thread
@@ -14,11 +16,18 @@ def start_telegram_bot():
     try:
         loop.run_until_complete(bot.start_bot())
     except Exception as e:
+        bot.send_to_discord(f"Error in Telegram bot: {e}")
         print(f"Error in Telegram bot: {e}")
     finally:
         loop.close()
 
+#send to discord
+def send_to_discord(content: str) -> None:
+        data = {"content": content}
+        response = requests.post(Config.DISCORD_WEBHOOK_URL, json=data, timeout=10)
+
 def main():
+    send_to_discord("Telegram copy trader started...")
     print("Welcome to the telegram copy trader...")
 
     # Create and start a new thread for the Telegram bot
@@ -29,6 +38,10 @@ def main():
         # Keep the main thread alive
         while telegram_thread.is_alive():
             time.sleep(1)  # Sleep to reduce CPU usage
+        else:
+            
+            print("Telegram bot thread has terminated.")
+            main()
     except Exception as e:
         print(e)
         print("Failed to start the Telegram bot.")
